8fec260b4b666c39fcddc3eab6e78ee88fe2e042
Hackbright Student <info@hackbrightacademy.com>
2016-07-26 12:58:48 -0700

    completed query for projects

--- hackbright.py
+++ hackbright.py
@@ -37,13 +37,24 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """
+        INSERT INTO Students VALUES (:first_name, :last_name, :github)"""
+    db.session.execute(QUERY,{'first_name': first_name, 'last_name': last_name,
+                             'github': github})
+    db.session.commit()
+
+    print "Successfully added student: %s %s" % (first_name, last_name)
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
-
+    QUERY = """ 
+                SELECT title, description, max_grade
+                FROM projects
+                WHERE title = :title """
+    db_cursor = db.session.execute(QUERY, {'title': title})
+    row = db_cursor.fetchone()
+    print "Project %s has a max grade of %s and is described below:\n%s" % (row[0], row[2], row[1])
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
@@ -74,8 +85,14 @@ def handle_input():
             get_student_by_github(github)
 
         elif command == "new_student":
-            first_name, last_name, github = args   # unpack!
+            first_name=args[0]
+            last_name=args[1]
+            github= args[2]  
             make_new_student(first_name, last_name, github)
+
+        elif command == "project":
+            title = args[0]
+            get_project_by_title(title)
 
         else:
             if command != "quit":
@@ -86,6 +103,6 @@ if __name__ == "__main__":
     app = Flask(__name__)
     connect_to_db(app)
 
-    # handle_input()
+    handle_input()
 
     db.session.close()
